b087f727b77c8bcecbe02f3cd8fbdf4362b96329
white-source <facevoidxk@gmail.com>
2020-01-19 00:47:16 +0800

    init

--- learn-python-struct/exercise-5.py
+++ learn-python-struct/exercise-5.py
@@ -10,7 +10,7 @@ randomListData = [1, 3, 21, 16, 2, 6, 22, 10, 7, 10]
 
 # 1 遍历 两两交换 找出最大的那个 元素 放在后面
 # 2 先写内层的循环，再写外层的循环
-# 3 学习可视化网站 https://visualgo.net/en/sorting
+# 3  学习可视化网站 https://visualgo.net/en/sorting
 def bubblesort(randomList):
     for j in range(len(randomList)):
         for i in range(len(randomList)-j):
